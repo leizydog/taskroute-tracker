9d7948b47b866f1de31faa3e68109434b66a5b65
leizydog <kmaghinang57@gmail.com>
2025-12-04 02:16:07 +0800

    revision number 3 and 4

--- frontend/src/components/organisms/index.js
+++ frontend/src/components/organisms/index.js
@@ -1,14 +1,56 @@
+// src/components/organisms/index.js
+
+// CreateTaskModal
 export * from './CreateTaskModal';
+export { default as CreateTaskModal } from './CreateTaskModal';
+
+// EmployeeKPIPanel
 export * from './EmployeeKPIPanel';
+export { default as EmployeeKPIPanel } from './EmployeeKPIPanel';
+
+// EmployeeSelector
 export * from './EmployeeSelector';
+export { default as EmployeeSelector } from './EmployeeSelector';
+
+// LiveLocationTracker
+export * from './LiveLocationTracker';
 export { default as LiveLocationTracker } from './LiveLocationTracker';
+
+// MLRecommendationPanel
 export * from './MLRecommendationPanel';
+export { default as MLRecommendationPanel } from './MLRecommendationPanel';
+
+// PerformanceComparisonPanel
 export * from './PerformanceComparisonPanel';
+export { default as PerformanceComparisonPanel } from './PerformanceComparisonPanel';
+
+// TaskManagementPanel
 export * from './TaskManagementPanel';
+export { default as TaskManagementPanel } from './TaskManagementPanel';
+
+// TaskRoutePlanner
 export * from './TaskRoutePlanner';
+export { default as TaskRoutePlanner } from './TaskRoutePlanner';
+
+// AdvancedMarker
+export * from './AdvancedMarker';
+export { default as AdvancedMarker } from './AdvancedMarker';
+
+// TaskForecast
+export * from './TaskForecast';
+export { default as TaskForecast } from './TaskForecast';
+
+// TaskDetailsModal
+export * from './TaskDetailsModal';
+export { default as TaskDetailsModal } from './TaskDetailsModal';
+
+// EditTaskModal
+export * from './EditTaskModal';
+export { default as EditTaskModal } from './EditTaskModal';
+
+// ---------------------------------------------------------
+// NOTE: I left these as single exports because they are 
+// likely folders, not single components.
+// ---------------------------------------------------------
 export * from '../atoms';
 export * from '../molecules';
-export * from './AdvancedMarker';
-export * from './TaskForecast';
-export * from './TaskDetailsModal';
-export * from './EditTaskModal';
